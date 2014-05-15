0e264208d6dd98c97a61f9f370a9c827369d8553
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-05-14 22:19:55 -0300

    Scaffolding: jquery+bootstrap+angular+d3+topojson

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('vonocabaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
