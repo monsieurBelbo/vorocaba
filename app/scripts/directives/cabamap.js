e1c529f0a1a9e89558ff521a1e65f10af8b199e2
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-05-19 20:57:55 -0300

    Se usa una escala lineal para pintar las regiones de Voronoi. Closes #1

--- app/scripts/directives/cabamap.js
+++ app/scripts/directives/cabamap.js
@@ -12,7 +12,9 @@ angular.module('vorocabaApp')
                 var width = attrs.width,
                     height = attrs.height;
 
-                var fill = d3.scale.category10();
+                var fill = d3.scale.linear()
+                    .domain([0, 10000])
+                    .range(["#fff", "#f00"]);
 
                 var projection = d3.geo.mercator()
                     .scale(190000)
@@ -28,51 +30,46 @@ angular.module('vorocabaApp')
                     .attr("width", width)
                     .attr("height", height);
 
+                var exterior;
+
                 scope.redraw = function() {
                     svg.selectAll(".points").remove();
                     svg.selectAll(".voronoi").remove();
                     $q.all([
-                        $http.get(scope.dataset.url)
-                    ]).then(drawVoronoi);
+                            $http.get(scope.dataset.url)
+                        ]).then(function(response) {
+                            drawVoronoi(scope.dataset.parse(response[0].data))
+                        });
                 }
 
                 $q.all([
-                    $http.get("/data/caba.json"),
-                    $http.get("/data/comisarias.csv")
-                ]).then(ready)
+                        $http.get("/data/caba.json")
+                    ]).then(function(response) {
+                        ready(response[0].data)
+                    })
 
-                function ready(response) {
-                    var caba = response[0].data;
+                function ready(caba) {
+                    var limits = topojson.feature(caba, caba.objects.limites);
 
-                    var defs = svg.append("defs");
+                    exterior = projectLineString(limits, projection);
 
-                    defs.append("path")
-                        .datum(topojson.feature(caba, caba.objects.limites))
-                        .attr("id", "limits")
+                    svg.append("path")
+                        .datum(limits)
+                        .attr("class", "limits")
                         .attr("d", path);
-
-                    defs.append("clipPath")
-                        .attr("id", "clip")
-                        .append("use")
-                        .attr("xlink:href", "#limits");
-
-                    svg.append("use")
-                        .attr("xlink:href", "#limits")
-                        .attr("class", "limits");
 
                     svg.append("path")
                         .datum(topojson.mesh(caba, caba.objects.barrios))
                         .attr("class", "subunit")
-                        .attr("d", path)
+                        .attr("d", path);
                 }
 
                 function polygon(d) {
                     return "M" + d.join("L") + "Z";
                 }
 
-                function drawVoronoi(response) {
-                    var datapoints = scope.dataset.parse(response[0].data),
-                        coordinates = datapoints.map(function(d) { return [+d[scope.dataset.long], +d[scope.dataset.lat]]; });
+                function drawVoronoi(points) {
+                    var coordinates = points.map(function(d) { return [+d[scope.dataset.long], +d[scope.dataset.lat]]; });
 
                     svg.append("path")
                         .datum({type: "MultiPoint", coordinates: coordinates})
@@ -80,18 +77,38 @@ angular.module('vorocabaApp')
                         .attr("d", path)
 
                     if (scope.dataset.voronoi) {
-                        var voronoiLayer = svg.append("g")
+                        svg.append("g")
                             .attr("class", "land")
-                            .attr("clip-path", "url(#clip)")
-
-                        voronoiLayer.selectAll(".voronoi")
-                            .data(voronoi(coordinates.map(projection)))
+                            .selectAll(".voronoi")
+                            .data(voronoi(coordinates.map(projection)).map(function(d) {
+                                // Each voronoi region is a convex polygon, therefore we can use
+                                // d3.geom.polygon.clip, treating each regino as a clip region, with the
+                                // projected “exterior” as a subject polygon.
+                                return d3.geom.polygon(d).clip(exterior.slice());
+                            }))
                             .enter().append("path")
                             .attr("class", "voronoi")
-                            .style("fill", function(d,i) { return fill(i) })
-                            .attr("d", polygon)
+                            .style("fill", function(d) { return fill(Math.abs(d3.geom.polygon(d).area())); })
+                            .attr("d", polygon);
                     }
                 }
+
+                // Extracts a single LineString from the given feature, projected (and resampled) using the given projection.
+                // by @jasondavies
+                function projectLineString(feature, projection) {
+                    var line;
+                    d3.geo.stream(feature, projection.stream({
+                        polygonStart: noop,
+                        polygonEnd: noop,
+                        lineStart: function() { line = []; },
+                        lineEnd: noop,
+                        point: function(x, y) { line.push([x, y]); },
+                        sphere: noop
+                    }));
+                    return line;
+                }
+
+                function noop() {}
             }
         };
     });
